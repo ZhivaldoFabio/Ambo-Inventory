adde5edc999f4278e5f2e9edb4be7c01077646fc
ZhivaldoFabio <zhivaldo.fabio@ti.ukdw.ac.id>
2024-10-29 03:06:59 +0700

    Update Sidebar.vue

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,102 +1,94 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '@/firebase';
 
-const isSidebarClosed = ref(false);
-const activeSubMenu = ref(null); // To track the currently active sub-menu
+const currentUser = ref(null); // Track the current logged-in User.
 
-// Toggle the sidebar open/close state
-function toggleSidebar() {
-  isSidebarClosed.value = !isSidebarClosed.value;
-}
-
-// Toggle the sub-menu
-function toggleSubMenu(index) {
-  if (activeSubMenu.value === index) {
-    activeSubMenu.value = null; // Close the currently active sub-menu
-  } else {
-    activeSubMenu.value = index; // Set the clicked menu as active
-  }
-}
+// On mount, fetch the logged-in user's info
+onMounted(() => {
+  onAuthStateChanged(auth, (user) => {
+    currentUser.value = user || null;
+  });
+});
 </script>
 
 <template>
   <nav
     :class="[
-      
       'flex',
       'min-h-screen',
-      isSidebarClosed ? 'w-16' : '',
+
       'transition-all',
       'duration-300',
       'h-full',
     ]"
     class="m-2"
   >
-    <ul class="p-4 rounded-md">
-      <!-- SIDEBAR TITLE -->
-      <li class="text-primary-300 flex items-center space-x-2">
-        <i class="pi pi-user"></i>
-        <span class="text-background-300"> Admin </span>
-      </li>
+    <div>
+      <ul
+        v-if="currentUser && currentUser.uid === 'RNE0WPnwrUcSH1m3vOmN3EVJ2ks2'"
+        class="p-4 rounded-md"
+      >
+        <!-- SIDEBAR TITLE -->
+        <li class="text-primary-300 flex items-center space-x-2">
+          <i class="pi pi-user"></i>
+          <span class="text-background-300"> Admin </span>
+        </li>
 
-      <li class="p-1 ml-5">
-        <RouterLink to="/home" class="flex items-center space-x-2">
-          <span>Dashboard</span>
-        </RouterLink>
-      </li>
-      <li class="p-1 ml-5">
-        <RouterLink to="/laporanpembelian" class="flex items-center space-x-2">
-          <span>Laporan Pembelian</span>
-        </RouterLink>
-      </li>
-      <li class="p-1 ml-5">
-        <RouterLink to="/laporanpenjualan" class="flex items-center space-x-2">
-          <span>Laporan Penjualan</span>
-        </RouterLink>
-      </li>
-
-      <!-- SIDEBAR TITLE -->
-      <li class="text-primary-300 flex items-center space-x-2 mt-5">
-        <i class="pi pi-warehouse"></i>
-        <span class="text-background-300"> Gudang </span>
-      </li>
-      <li class="p-1 ml-5">
-        <RouterLink to="/home" class="flex items-center space-x-2">
-          <span>Produk</span>
-        </RouterLink>
-      </li>
-      <li class="p-1 ml-5">
-        <RouterLink to="/home" class="flex items-center space-x-2">
-          <span>Unit</span>
-        </RouterLink>
-      </li>
-      <li class="p-1 ml-5">
-        <RouterLink to="/home" class="flex items-center space-x-2">
-          <span>Kategori</span>
-        </RouterLink>
-      </li>
-      <li class="p-1 ml-5">
-        <RouterLink to="/home" class="flex items-center space-x-2">
-          <span>Pembelian</span>
-        </RouterLink>
-      </li>
-    </ul>
+        <li
+          class="p-1 ml-5 hover:shadow-lg hover:shadow-background-200 rounded-lg"
+        >
+          <RouterLink to="/home" class="flex items-center space-x-2">
+            <span>Dashboard</span>
+          </RouterLink>
+        </li>
+        <li class="p-1 ml-5">
+          <RouterLink
+            to="/laporanpembelian"
+            class="flex items-center space-x-2"
+          >
+            <span>Laporan Pembelian</span>
+          </RouterLink>
+        </li>
+        <li class="p-1 ml-5">
+          <RouterLink
+            to="/laporanpenjualan"
+            class="flex items-center space-x-2"
+          >
+            <span>Laporan Penjualan</span>
+          </RouterLink>
+        </li>
+      </ul>
+      <ul class="p-4 rounded-md">
+        <!-- SIDEBAR TITLE -->
+        <li class="text-primary-300 flex items-center space-x-2 mt-5">
+          <i class="pi pi-warehouse"></i>
+          <span class="text-background-300"> Gudang </span>
+        </li>
+        <li class="p-1 ml-5">
+          <RouterLink to="/home" class="flex items-center space-x-2">
+            <span>Produk</span>
+          </RouterLink>
+        </li>
+        <li class="p-1 ml-5">
+          <RouterLink to="/home" class="flex items-center space-x-2">
+            <span>Unit</span>
+          </RouterLink>
+        </li>
+        <li class="p-1 ml-5">
+          <RouterLink to="/home" class="flex items-center space-x-2">
+            <span>Kategori</span>
+          </RouterLink>
+        </li>
+        <li class="p-1 ml-5">
+          <RouterLink to="/home" class="flex items-center space-x-2">
+            <span>Pembelian</span>
+          </RouterLink>
+        </li>
+      </ul>
+    </div>
   </nav>
 </template>
 
-<style scoped>
-/* Add your sidebar styles here */
-.close {
-  width: 60px;
-  /* Any other styles to make the sidebar "closed" */
-}
-
-/* Active submenu styling */
-.sub-menu {
-  display: none;
-}
-
-.sub-menu.show {
-  display: block;
-}
-</style>
+<style scoped></style>
