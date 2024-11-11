17a668359810beef7bce2d672020f799b0f14bbd
chelle1003 <79003852+chelle1003@users.noreply.github.com>
2024-11-12 01:38:45 +0700

    asd

--- depotesambo.sql
+++ depotesambo.sql
@@ -15,6 +15,7 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 -- Dumping structure for table depotesambo.detailpenjualan
+DROP TABLE IF EXISTS `detailpenjualan`;
 CREATE TABLE IF NOT EXISTS `detailpenjualan` (
   `id_detail_penjualan` int NOT NULL AUTO_INCREMENT,
   `id_produk` int NOT NULL,
@@ -29,13 +30,14 @@ CREATE TABLE IF NOT EXISTS `detailpenjualan` (
 DELETE FROM `detailpenjualan`;
 
 -- Dumping structure for table depotesambo.kategori
+DROP TABLE IF EXISTS `kategori`;
 CREATE TABLE IF NOT EXISTS `kategori` (
   `id_kategori` int NOT NULL AUTO_INCREMENT,
   `nama_kategori` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   PRIMARY KEY (`id_kategori`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
--- Dumping data for table depotesambo.kategori: ~6 rows (approximately)
+-- Dumping data for table depotesambo.kategori: ~3 rows (approximately)
 DELETE FROM `kategori`;
 INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
 	(1, 'Shampo'),
@@ -46,26 +48,31 @@ INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
 	(6, 'Lilin');
 
 -- Dumping structure for table depotesambo.pembelian
+DROP TABLE IF EXISTS `pembelian`;
 CREATE TABLE IF NOT EXISTS `pembelian` (
   `id_pembelian` int NOT NULL AUTO_INCREMENT,
   `jumlah_produk` int NOT NULL,
   `tanggal_pembelian` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `id_produk` int NOT NULL,
   `id_supplier` int NOT NULL,
+  `id_unit` int NOT NULL,
   PRIMARY KEY (`id_pembelian`),
   KEY `id_produk` (`id_produk`),
   KEY `id_supplier` (`id_supplier`),
+  KEY `pembelian_ibfk_3` (`id_unit`),
   CONSTRAINT `pembelian_ibfk_1` FOREIGN KEY (`id_produk`) REFERENCES `produk` (`id_produk`) ON UPDATE CASCADE,
-  CONSTRAINT `pembelian_ibfk_2` FOREIGN KEY (`id_supplier`) REFERENCES `suppliers` (`id_supplier`) ON UPDATE CASCADE
+  CONSTRAINT `pembelian_ibfk_2` FOREIGN KEY (`id_supplier`) REFERENCES `suppliers` (`id_supplier`) ON UPDATE CASCADE,
+  CONSTRAINT `pembelian_ibfk_3` FOREIGN KEY (`id_unit`) REFERENCES `units` (`id_unit`) ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table depotesambo.pembelian: ~2 rows (approximately)
 DELETE FROM `pembelian`;
-INSERT INTO `pembelian` (`id_pembelian`, `jumlah_produk`, `tanggal_pembelian`, `id_produk`, `id_supplier`) VALUES
-	(1, 42, '2024-11-06 16:56:14', 4, 3),
-	(2, 48, '2024-11-06 16:56:14', 3, 3);
+INSERT INTO `pembelian` (`id_pembelian`, `jumlah_produk`, `tanggal_pembelian`, `id_produk`, `id_supplier`, `id_unit`) VALUES
+	(1, 42, '2024-11-06 16:56:14', 4, 3, 1),
+	(2, 48, '2024-11-06 16:56:14', 3, 3, 1);
 
 -- Dumping structure for table depotesambo.penjualan
+DROP TABLE IF EXISTS `penjualan`;
 CREATE TABLE IF NOT EXISTS `penjualan` (
   `id_penjualan` varchar(50) NOT NULL,
   `id_detail_penjualan` int NOT NULL,
@@ -80,6 +87,7 @@ CREATE TABLE IF NOT EXISTS `penjualan` (
 DELETE FROM `penjualan`;
 
 -- Dumping structure for table depotesambo.produk
+DROP TABLE IF EXISTS `produk`;
 CREATE TABLE IF NOT EXISTS `produk` (
   `id_produk` int NOT NULL AUTO_INCREMENT,
   `nama_produk` varchar(255) NOT NULL,
@@ -89,24 +97,28 @@ CREATE TABLE IF NOT EXISTS `produk` (
   `id_unit` int NOT NULL,
   `isi_produk` int NOT NULL,
   `stock_minimum` int DEFAULT NULL,
+  `id_supplier` int NOT NULL,
   PRIMARY KEY (`id_produk`) USING BTREE,
   KEY `id_kategori` (`id_kategori`),
   KEY `id_unit` (`id_unit`),
+  KEY `produk_ibfk_3` (`id_supplier`),
   CONSTRAINT `produk_ibfk_1` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`) ON UPDATE CASCADE,
-  CONSTRAINT `produk_ibfk_2` FOREIGN KEY (`id_unit`) REFERENCES `units` (`id_unit`) ON UPDATE CASCADE
+  CONSTRAINT `produk_ibfk_2` FOREIGN KEY (`id_unit`) REFERENCES `units` (`id_unit`) ON UPDATE CASCADE,
+  CONSTRAINT `produk_ibfk_3` FOREIGN KEY (`id_supplier`) REFERENCES `suppliers` (`id_supplier`) ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Dumping data for table depotesambo.produk: ~6 rows (approximately)
 DELETE FROM `produk`;
-INSERT INTO `produk` (`id_produk`, `nama_produk`, `harga_beli`, `harga_jual`, `id_kategori`, `id_unit`, `isi_produk`, `stock_minimum`) VALUES
-	(1, 'Shampo Emeron hijau kecil', 14000, 22000, 1, 1, 24, 15),
-	(2, 'Shampo Emeron hijau besar', 17000, 28000, 1, 1, 24, 13),
-	(3, 'Kecap tomat', 8500, 14000, 5, 1, 6, 12),
-	(4, 'Kecap payung', 8000, 13500, 5, 1, 6, 11),
-	(5, 'Sabun Lifebuoy btg lemon', 3500, 5000, 2, 1, 4, 50),
-	(6, 'Sabun Lifebuoy btg merah', 3500, 5000, 2, 1, 4, 70);
+INSERT INTO `produk` (`id_produk`, `nama_produk`, `harga_beli`, `harga_jual`, `id_kategori`, `id_unit`, `isi_produk`, `stock_minimum`, `id_supplier`) VALUES
+	(1, 'Shampo Emeron hijau kecil', 14000, 22000, 1, 1, 24, 15, 2),
+	(2, 'Shampo Emeron hijau besar', 17000, 28000, 1, 1, 24, 13, 2),
+	(3, 'Kecap tomat', 8500, 14000, 5, 1, 6, 12, 2),
+	(4, 'Kecap payung', 8000, 13500, 5, 1, 6, 11, 2),
+	(5, 'Sabun Lifebuoy btg lemon', 3500, 5000, 2, 1, 4, 50, 2),
+	(6, 'Sabun Lifebuoy btg merah', 3500, 5000, 2, 1, 4, 70, 2);
 
 -- Dumping structure for table depotesambo.stock
+DROP TABLE IF EXISTS `stock`;
 CREATE TABLE IF NOT EXISTS `stock` (
   `id_stock` int NOT NULL AUTO_INCREMENT,
   `id_unit` int NOT NULL,
@@ -125,7 +137,7 @@ CREATE TABLE IF NOT EXISTS `stock` (
   CONSTRAINT `stock_ibfk_2` FOREIGN KEY (`id_produk`) REFERENCES `produk` (`id_produk`) ON UPDATE CASCADE,
   CONSTRAINT `stock_ibfk_3` FOREIGN KEY (`id_supplier`) REFERENCES `suppliers` (`id_supplier`) ON UPDATE CASCADE,
   CONSTRAINT `stock_ibfk_4` FOREIGN KEY (`id_unit`) REFERENCES `units` (`id_unit`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table depotesambo.stock: ~4 rows (approximately)
 DELETE FROM `stock`;
@@ -133,9 +145,10 @@ INSERT INTO `stock` (`id_stock`, `id_unit`, `id_kategori`, `id_produk`, `id_supp
 	(1, 1, 1, 1, 2, 189, '2026-11-27', '2024-01-10'),
 	(2, 1, 2, 5, 6, 13, '2024-11-11', '2024-11-10'),
 	(3, 1, 2, 6, 5, 14, '2025-11-10', '2024-11-10'),
-	(9, 1, 1, 1, 3, 5, '2025-11-11', '2024-11-11');
+	(10, 3, 5, 3, 2, 10, '2025-11-11', '2024-11-11');
 
 -- Dumping structure for table depotesambo.suppliers
+DROP TABLE IF EXISTS `suppliers`;
 CREATE TABLE IF NOT EXISTS `suppliers` (
   `id_supplier` int NOT NULL AUTO_INCREMENT,
   `nama_supplier` varchar(50) NOT NULL,
@@ -156,6 +169,7 @@ INSERT INTO `suppliers` (`id_supplier`, `nama_supplier`, `alamat`, `email`, `no_
 	(7, 'Layla', 'jl rajawali no 31', 'laylakc@gmail.com', '08218998446');
 
 -- Dumping structure for table depotesambo.units
+DROP TABLE IF EXISTS `units`;
 CREATE TABLE IF NOT EXISTS `units` (
   `id_unit` int NOT NULL AUTO_INCREMENT,
   `nama_unit` varchar(50) NOT NULL,
@@ -170,6 +184,7 @@ INSERT INTO `units` (`id_unit`, `nama_unit`) VALUES
 	(3, 'Box');
 
 -- Dumping structure for table depotesambo.users
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id_user` int NOT NULL AUTO_INCREMENT,
   `nama_user` varchar(20) NOT NULL,
